e4312f654f525851fe0618aeb2bf6727d957130f
bryan <bryan.griffin@sonic.net>
2026-02-14 16:50:04 -0800

    B: Created moving arrow

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids with pygame version: VERSION")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    fps = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         log_state()
-        for event in pygame.event.get():
-            pass
         screen.fill("black")
+        for drawn in drawable:
+            drawn.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = fps.tick(60) / 1000
+
 if __name__ == "__main__":
     main()
